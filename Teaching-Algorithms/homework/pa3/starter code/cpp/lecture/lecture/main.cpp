569667a7e6723d7cd9417016ec19f0a06c24373b
Ashleigh Robie <acr532@humboldt.edu>
2019-03-09 14:41:49 -0800

    tier 2 in progress

--- Teaching-Algorithms/homework/pa3/starter code/cpp/lecture/lecture/main.cpp
+++ Teaching-Algorithms/homework/pa3/starter code/cpp/lecture/lecture/main.cpp
@@ -6,6 +6,7 @@ int main(void)
 {
 	unordered_map<string, unordered_map<string, int>> completeMap;
 	CityGraph graph{};
+	CityGraph Tier2graph{};
 	string mapFile, deliveriesFile;
 	cout << "***Route Planner***" << endl;
 	cout << "Enter maps file: "; 
@@ -21,7 +22,6 @@ int main(void)
 		currentPath = _completeMap[i];
 		graph.addVertex(currentPath[0]);
 		graph.addVertex(currentPath[1]);
-	
 	}
 
 	for (int i = 0; i < _completeMap.size(); i++)
@@ -37,21 +37,52 @@ int main(void)
 	vector<vector<string>> deliveryMap = _deliveriesfile.processFile();
 	// calculate all possible route using MST algorithm
 
-	vector<string> currentHouse = deliveryMap[0];
-	auto routes = graph.computeMinimumSpanningTree(currentHouse[0]);
+	vector<string> startHouse = deliveryMap[0];
+	CityGraph MSTgraph = graph;
+	auto routes = MSTgraph.computeMinimumSpanningTree(startHouse[0]);
+
 	
+
+	// ======== TIER 2 ==========
+	unordered_map<string, int> distances{};
+	int route_weight = 0;
+
+	// reduce map then calculate shortest path
+	// loads vertexes into single vector
+	vector<string> deliveries;
+	for (int v = 0; v < deliveryMap.size(); v++)
+	{
+		vector<string> current = deliveryMap[v];
+		deliveries.push_back(current[0]);
+	}
+
+	// calculate shortest path from 
+	for (int c = 0; c < deliveries.size(); c++)
+	{
+		distances = graph.computeShortestPath(deliveries[c]);
+
+		for (auto kvp : distances)
+		{
+			if (kvp.first == deliveries[c+1])
+			{
+				Tier2graph.addVertex(kvp.first);
+				route_weight = kvp.second;
+				Tier2graph.connectVertex(deliveries[c], deliveries[c + 1], route_weight);
+			}
+		}
+	}
+
+	// ======== Tier 1 =========
 	// compute minimum spanning trees returns a vector of Edges
 	// sum cost of edges
 	Edge currentEdge;
-	int routeTime = 0;
+	int MSTrouteTime = 0;
 	for (int s = 0; s < routes.size(); s++)
 	{
 		currentEdge = routes[s];
-		routeTime += currentEdge.weight;
+		MSTrouteTime += currentEdge.weight;
 	}
-
-	cout << "Total travel time: " << routeTime << " minutes" << endl;
-	// calculate total time it will take
+	cout << "Total travel time for MST: " << MSTrouteTime << " minutes" << endl;
 
  
 	return 0;
